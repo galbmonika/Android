e7a7239a4e003d763952b1cca68e1238082857ed
galbmonika <galbmonka@gmail.com>
2016-10-13 21:09:09 +0200

    talán utolsó verzió mára

--- app/src/main/java/com/example/bylm3t/jarvis/MainActivity.java
+++ app/src/main/java/com/example/bylm3t/jarvis/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.bylm3t.jarvis;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
@@ -12,16 +14,32 @@ public class MainActivity extends AppCompatActivity {
     private ListView list;
     private EditText text;
     private MessageAdapter adapter;
+    private Button send;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         text= (EditText) findViewById(R.id.editText);
         list= (ListView) findViewById(R.id.list);
+
+        send=(Button) findViewById(R.id.send);
         adapter=new MessageAdapter(this, new ArrayList<String>());
         adapter.add(getString(R.string.hello));
         list.setAdapter(adapter);
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String str=text.getText().toString();
+                adapter.add(text.getText().toString());
+                text.setText("");
+                respond(str);
+            }
+        });
 
+    }
 
+    private void respond(String str) {
+        adapter.add(str);
     }
 }
