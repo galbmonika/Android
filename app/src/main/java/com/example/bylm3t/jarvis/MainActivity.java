b227aaee8f398b104ffedc0180db7c397cf37dcb
galbmonika <galbmonka@gmail.com>
2016-10-13 21:00:40 +0200

    commitpush

--- app/src/main/java/com/example/bylm3t/jarvis/MainActivity.java
+++ app/src/main/java/com/example/bylm3t/jarvis/MainActivity.java
@@ -5,16 +5,23 @@ import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     private ListView list;
     private EditText text;
+    private MessageAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         text= (EditText) findViewById(R.id.editText);
         list= (ListView) findViewById(R.id.list);
+        adapter=new MessageAdapter(this, new ArrayList<String>());
+        adapter.add(getString(R.string.hello));
+        list.setAdapter(adapter);
+
 
     }
 }
--- app/src/main/java/com/example/bylm3t/jarvis/MessageAdapter.java
+++ app/src/main/java/com/example/bylm3t/jarvis/MessageAdapter.java
@@ -1,7 +1,14 @@
 package com.example.bylm3t.jarvis;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
 
 /**
  * Created by BYLM3T on 2016.10.13..
@@ -9,7 +16,17 @@ import android.widget.ArrayAdapter;
 
 public class MessageAdapter extends ArrayAdapter<String>
 {
-    public MessageAdapter(Context context, String[] objects){
+    public MessageAdapter(Context context, List<String> objects){
         super(context, R.layout.message, objects);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        String msg=getItem(position);
+        convertView= LayoutInflater.from(getContext()).inflate(R.layout.message,parent,false);
+        TextView t=(TextView) convertView.findViewById(R.id.text);
+        t.setText(msg);
+        return convertView;
     }
 }
